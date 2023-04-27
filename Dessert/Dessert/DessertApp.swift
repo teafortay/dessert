4e6224a5cf0f1ee8ba2905e0055a348a48f57da7
Taylor Shaw <tay@taylorcodeshop.com>
2023-04-27 14:05:26 -0400

    created xcode project

---
+++ Dessert/Dessert/DessertApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertApp.swift
+//  Dessert
+//
+//  Created by Taylor Shaw on 4/27/23.
+//
+
+import SwiftUI
+
+@main
+struct DessertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
