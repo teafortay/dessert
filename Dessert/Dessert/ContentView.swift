30f97d8fc802f7ca58ca01826db471e627672141
Taylor Shaw <tay@taylorcodeshop.com>
2023-04-29 02:05:04 -0400

    added api calls + mvvm + combine, compiles but not displaying data

--- Dessert/Dessert/ContentView.swift
+++ Dessert/Dessert/ContentView.swift
@@ -6,8 +6,84 @@
 //
 
 import SwiftUI
+import Combine
+
+class DessertViewModel: ObservableObject {
+    @Published var dataSource: DessertRowViewModel?
+    private var disposables = Set<AnyCancellable>()
+    
+    func refresh() {
+      decode()
+        .map(DessertRowViewModel.init)
+        .receive(on: DispatchQueue.main)
+        .sink(receiveCompletion: { [weak self] value in
+          guard let self = self else { return }
+          switch value {
+          case .failure:
+            self.dataSource = nil
+          case .finished:
+            break
+          }
+        }, receiveValue: { [weak self] dessert in
+            guard let self = self else { return }
+            self.dataSource = dessert
+        })
+        .store(in: &disposables)
+    }
+}
+
+struct DessertRowViewModel {
+    private let item: DessertModel
+    init(item: DessertModel) {
+      self.item = item
+    }
+}
+struct DessertView: View {
+    @ObservedObject var viewModel: DessertViewModel
+    
+    init(viewModel: DessertViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    var body: some View {
+        List(content: content)
+            .onAppear(perform: viewModel.refresh)
+    }
+}
+
+private extension DessertView {
+  func content() -> some View {
+    if let viewModel = viewModel.dataSource {
+      return AnyView(details(for: viewModel))
+    } else {
+      return AnyView(loading)
+    }
+  }
+
+  func details(for viewModel: DessertRowViewModel) -> some View {
+    DessertRow(viewModel: viewModel)
+  }
+
+  var loading: some View {
+    Text("Loadings weather...")
+      .foregroundColor(.gray)
+  }
+}
+
+struct DessertRow: View {
+    private let viewModel: DessertRowViewModel
+    
+    init(viewModel: DessertRowViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    var body: some View {
+        Text("yummy dessert")
+    }
+}
 
 struct ContentView: View {
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
--- Dessert/Dessert/DessertApp.swift
+++ Dessert/Dessert/DessertApp.swift
@@ -6,12 +6,45 @@
 //
 
 import SwiftUI
+import Combine
+
+enum APIError: Error {
+    case notFound
+    case downloadError
+    case decodingError
+}
+
+struct DessertModel: Decodable, Equatable {
+  let strMeal: String
+  let strMealThumb: String
+  let idMeal: Int
+}
+
+extension DessertModel: Identifiable {
+  var id: String { strMeal }
+}
+
+func decode() -> AnyPublisher<DessertModel, APIError> {
+  guard let url = URL(string: "https://themealdb.com/api/json/v1/1/filter.php?c=Dessert") else {
+    fatalError("Error on creating url")
+  }
+    let decoder = JSONDecoder()
+    decoder.keyDecodingStrategy = .convertFromSnakeCase
+  return URLSession.shared.dataTaskPublisher(for: url)
+    .mapError { _ in APIError.downloadError }
+    .map { data, _ in data }
+    .decode(type: DessertModel.self, decoder: decoder)
+    .mapError { _ in APIError.decodingError }
+    .eraseToAnyPublisher()
+}
 
 @main
 struct DessertApp: App {
+    let viewmodel = DessertViewModel()
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            DessertView(viewModel: viewmodel)
+            
         }
     }
 }
